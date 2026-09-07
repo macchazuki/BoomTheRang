8c1163d8588f77b02ea18fd620276ee08d936b17
macchazuki <32705562+macchazuki@users.noreply.github.com>
2026-09-07 04:16:46 +0000

    Deploying to gh-pages from @ macchazuki/BoomTheRang@79dcd2222857621c8fa2c4d4e4216720fa2ff8e0 🚀

--- src/game.js
+++
@@ -1,9 +0,0 @@
-export const GAME_NAME = 'BoomTheRang';
-
-export function clamp(value, minimum, maximum) {
-  if (minimum > maximum) {
-    throw new RangeError('minimum must not be greater than maximum');
-  }
-
-  return Math.min(maximum, Math.max(minimum, value));
-}
